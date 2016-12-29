34be253732e0129758681ed5e978a3440d013698
bnoden <brandroid369@gmail.com>
2016-12-28 23:44:04 -0600

    .

---
+++ 07/07_04.cpp
@@ -0,0 +1,44 @@
+/*
+4. Larger Than n
+In a program, write a function that accepts three arguments: an array, the size of the 
+array, and a number n. Assume that the array contains integers. The function should 
+display all of the numbers in the array that are greater than the number n. 
+*/
+
+#include <iostream>
+using namespace std;
+
+void LargerThan(int[], int, int);
+
+// TEST
+void main() {
+	const int SIZE = 10;
+	int num[SIZE];
+	int number;
+
+	cout << "Enter 10 numbers." << endl;
+
+	for (int i = 0; i < SIZE; i++) {
+		cout << "Number " << i+1 << ": ";
+		cin >> num[i];
+	}
+
+	cout << "\nNow enter your \"special number\": ";
+	cin >> number;
+
+	cout << "\nThe following numbers are greater than your \"special number\": " << endl;
+	
+	LargerThan(num, SIZE, number);
+
+	cout << endl;
+
+	return;
+}
+
+void LargerThan(int arr[],int size,int n) {
+	for (int i = 0; i < size; i++) {
+		if (arr[i] > n) {
+			cout << arr[i] << endl;
+		}
+	}
+}
