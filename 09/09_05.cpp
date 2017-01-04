c66c75f28fc5c4ff28b7586786c1ed8acabd4ec0
bnoden <brandroid369@gmail.com>
2017-01-04 00:26:11 -0600

    .

---
+++ 09/09_05.cpp
@@ -0,0 +1,42 @@
+/*
+5. Pointer Rewrite 
+The following function uses reference variables as parameters. Rewrite the function so 
+it uses pointers instead of reference variables, and then demonstrate the function in a 
+complete program. 
+int doSomething(int &x, int &y) 
+{ 
+	int temp = x; 
+	x = y * 10; 
+	y = temp * 10; 
+	return x + y; 
+} 
+*/
+
+#include <iostream>
+using namespace std;
+
+int doSomething(int*, int*);
+
+void main() {
+	int *x;
+	int *y;
+	int z = 5;
+	y = &z;
+	x = &z;
+	
+	int something = doSomething(x,y);
+	for (int i = 0; i < something; i++) {
+		cout << "Doing something..." << endl;
+	}
+	cout << "Something was done " << something << " times." << endl;
+	cout << "\nGoodbye.\n" << endl;
+
+	return; 
+}
+
+int doSomething(int *x,int *y) {
+	int temp = *x;
+	*x = *y * 10;
+	*y = temp * 10;
+	return *x + *y;
+}
