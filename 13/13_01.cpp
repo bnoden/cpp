c055a4eae1179c731bf5ff9e0a548d806ff0f4e3
bnoden <brandroid369@gmail.com>
2017-01-07 03:34:16 -0600

    .

---
+++ 13/13_01.cpp
@@ -0,0 +1,23 @@
+/*
+1. Date 
+Design a class called Date. The class should store a date in three integers: month, day, 
+and year. There should be member functions to print the date in the following forms: 
+12/25/2014 
+December 25, 2014 
+25 December 2014 
+Demonstrate the class by writing a complete program implementing it. 
+
+Input Validation: Do not accept values for the day greater than 31 or less than 1. Do 
+not accept values for the month greater than 12 or less than 1
+*/
+
+#include <iostream>
+
+
+
+void main() {
+	
+
+
+	return; 
+}
