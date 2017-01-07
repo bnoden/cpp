39097b2fadaf254d363dcdee31dc2921c6801dab
bnoden <brandroid369@gmail.com>
2017-01-07 06:33:25 -0600

    .

--- 13/13_01.cpp
+++ 13/13_01.cpp
@@ -12,12 +12,68 @@ not accept values for the month greater than 12 or less than 1
 */
 
 #include <iostream>
+#include <string>
 
+using namespace std;
 
+class Date {
+private:
+	int month, day, year;
+
+public:
+	string months[12] = {"January","February","March","April","May","June","July",
+						"August","September","October","November","December"};
+
+	void setMonth(int m) {month = m;}
+	void setDay(int d) {day = d;}
+	void setYear(int y) {year = y;}
+	
+	int getMonth() const {return month;}
+	int getDay() const {return day;}
+	int getYear() const {return year;}
+	
+	int monthToInt(string m) {
+		int intMonth = 0;
+		for (int i = 0; i < 12; i++) {
+			if (m == months[i]) { intMonth = i; }
+		}
+		return intMonth;
+	}
+
+	string monthToString(int m) { return months[m-1]; }
+
+	void showDate1() { cout << month << '\\' << day << '\\' << year; }
+	void showDate2() { cout << monthToString(month) << ' ' << day << ", " << year; }
+	void showDate3() { cout << day << ' ' << monthToString(month) << ' ' << year; }
+};
 
 void main() {
-	
+	int monthNum = 0, dayNum = 0, yearNum = 0;
+	Date date;
 
+	while (dayNum < 1 || dayNum > 31) {
+		cout << "Enter day (1-31): ";
+		cin >> dayNum;
+	}
+	date.setDay(dayNum);
+
+	while (monthNum < 1 || monthNum > 12) {
+		cout << "Enter month (1-12): ";
+		cin >> monthNum;
+	}
+	date.setMonth(monthNum);
+
+	while (yearNum < 1000 || yearNum > 9999) {
+		cout << "Enter year: ";
+		cin >> yearNum;
+	}
+	date.setYear(yearNum);
+
+	cout << endl;
+	date.showDate1(); cout << endl;
+	date.showDate2(); cout << endl;
+	date.showDate3(); cout << endl;
+	cout << endl;
 
 	return; 
 }
