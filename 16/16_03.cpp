4e1d5c739f017b33d2faa47395bb3f4a86368ee9
bnoden <brandroid369@gmail.com>
2017-01-10 20:33:21 -0600

    .

---
+++ 16/16_03.cpp
@@ -0,0 +1,19 @@
+/*
+3. Minimum/Maximum Templates 
+Write  templates  for  the  two  functions minimum and maximum.  The minimum function  
+should  accept  two  arguments  and  return  the  value  of  the  argument  that  is  the  lesser  
+of the two. The  maximum function should accept two arguments and return the value 
+of  the  argument  that  is  the  greater  of  the  two.  Design  a  simple  driver  program  that  
+demonstrates the templates with various data types. 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void main() {
+
+
+
+	return; 
+}
