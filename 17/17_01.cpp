ae04c646a262ee85bfb749701426df736f03ba53
bnoden <brandroid369@gmail.com>
2017-01-11 01:17:33 -0600

    .

---
+++ 17/17_01.cpp
@@ -0,0 +1,19 @@
+/*
+1. Your Own Linked List 
+Design  your  own  linked  list  class  to  hold  a  series  of  integers.  The  class  should  have  
+member functions for appending, inserting, and deleting nodes. Don’t forget to add a 
+destructor that destroys the list. Demonstrate the class with a driver program. 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+
+void main() {
+
+
+
+	return; 
+}
