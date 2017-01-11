7820e37d1cc34192bf4ad6382d221e1908cacd1b
bnoden <brandroid369@gmail.com>
2017-01-10 23:06:40 -0600

    .

--- 16/16_03.cpp
+++ 16/16_03.cpp
@@ -8,12 +8,43 @@ demonstrates the templates with various data types.
 */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+template <class T>
+T minimum(T val1,T val2) {
+	return val1 < val2 ? val1 : val2;
+}
+
+template <class T>
+T maximum(T val1,T val2) {
+	return val1 > val2 ? val1 : val2;
+}
+
 void main() {
+	int num1, num2;
+	string word1, word2;
+	
+	cout << "Enter two numbers." << endl;
+	cout << "First number: ";
+	cin >> num1;
+	cout << "Second number: ";
+	cin >> num2;
+	cout << endl;
+	cout << "The smallest is " << minimum(num1, num2) << " and the largest is "
+		<< maximum(num1, num2) << endl;
+	cout << endl;
+	
+	cout << "Enter two words." << endl;
+	cout << "First word: ";
+	cin >> word1;
+	cout << "Second word: ";
+	cin >> word2;
+	cout << endl;
+	cout << minimum(word1, word2) << " comes before " << maximum(word1, word2) << endl;
 
-
+	cout << endl;
 
 	return; 
 }
